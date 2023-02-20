6e9c9c658f3a2a55b462447955b3cca64c866060
Dmitriy_Korchagin <793070407232@ya.ru>
2023-02-20 17:44:17 +0300

    ьфшmain.java refactoring

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -2,14 +2,12 @@ package ru.netology;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
 import com.google.gson.reflect.TypeToken;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.ColumnPositionMappingStrategy;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.w3c.dom.*;
@@ -113,8 +111,6 @@ public class Main {
         }
         return null;
     }
-
-
 
     public static String readString(String fileName) {
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
